b97412e88cd0e068dcb7f6f646d41ce7ef476281
Matias Selin <selinmatias5@gmail.com>
2025-04-09 13:28:00 +0300

    form labels

--- ui/src/components/ServerRack.tsx
+++ ui/src/components/ServerRack.tsx
@@ -33,19 +33,25 @@ const ServerRack = ({ id, setServerRacks, serverRacks }: Props) => {
   return (
     <div className="grid grid-cols-2 gap-4 justify-center items-center">
       <div>
-        <input
-          type="text"
-          value={currentServerRack.name}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-        />
-        <input
-          type="number"
-          id="server-amount"
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-          placeholder="Amount of servers"
-          value={currentServerRack.serverAmount}
-          onChange={(e) => updateServerRacks(Number(e.target.value))}
-        />
+        <div className="flex justify-center items-center gap-2">
+          <label className="text-sm">Name</label>
+          <input
+            type="text"
+            value={currentServerRack.name}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <label className="text-sm">Server amount</label>
+          <input
+            type="number"
+            id="server-amount"
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            placeholder="Amount of servers"
+            value={currentServerRack.serverAmount}
+            onChange={(e) => updateServerRacks(Number(e.target.value))}
+          />
+        </div>
       </div>
     </div>
   )
