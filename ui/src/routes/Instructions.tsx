ebd8556ebccb97c9dcd9c6824758b4e00f1dc4fd
Jupsista <91373909+Jupsista@users.noreply.github.com>
2025-04-09 15:03:16 +0300

    Add instructions page

--- ui/src/routes/Instructions.tsx
+++ ui/src/routes/Instructions.tsx
@@ -1,5 +1,12 @@
 const Instructions = () => {
-  return <div>Here is info about how to use this app.</div>
+  return (
+    <div className="flex flex-col text-center h-screen text-white bg-linear-65 from-purple-500 to-pink-500">
+          <div className="m-auto">
+       <h1 className="text-xl">This is a server room modeling application with air conditioning</h1>
+        <p>To use the app, add servers using the server list on the right side in main view</p>
+          </div>
+    </div>
+  )
 }
 
 export default Instructions
