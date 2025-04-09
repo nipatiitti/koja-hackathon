25025a6397f1346dfc297705a8c8e2e6612a48f3
Matias Selin <selinmatias5@gmail.com>
2025-04-09 11:06:17 +0300

    react router

---
+++ ui/src/Instructions.tsx
@@ -0,0 +1,9 @@
+const Instructions = () => {
+    return (
+        <div>
+            Here is info about how to use this app.
+        </div>
+    )
+}
+
+export default Instructions;
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Routes, Route, BrowserRouter } from 'react-router'
+import Instructions from './Instructions.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<Instructions />} />
+      </Routes>
+    </StrictMode>,
+  </BrowserRouter>,
 )
