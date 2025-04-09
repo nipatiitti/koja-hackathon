19c0d991e2fd0283919f59b927681014ee1406ab
Matias Selin <selinmatias5@gmail.com>
2025-04-09 11:40:59 +0300

    styling, tailwind, etc.

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,19 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './components/App.tsx'
 import { Routes, Route, BrowserRouter } from 'react-router'
-import Instructions from './Instructions.tsx'
-import Navigation from './Navigation.tsx'
+import Instructions from './components/Instructions.tsx'
+import Navigation from './components/Navigation.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
-      <Navigation/>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<Instructions />} />
-      </Routes>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<Instructions />} />
+        </Routes>
+      </div>
     </StrictMode>
-  </BrowserRouter>
+  </BrowserRouter>,
 )
