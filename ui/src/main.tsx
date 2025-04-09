ae7852c76cd06bdf23c9dac6b8dc4fd7217a488d
Matias Selin <selinmatias5@gmail.com>
2025-04-09 11:15:13 +0300

    remove random commas :D

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -14,6 +14,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/about" element={<Instructions />} />
       </Routes>
-    </StrictMode>,
-  </BrowserRouter>,
+    </StrictMode>
+  </BrowserRouter>
 )
